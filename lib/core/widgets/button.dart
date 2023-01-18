68b2459aaa2c49f90de84e1615837e97c393f510
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-01-18 14:37:37 +0600

    color_config - v.1.0.0

--- lib/core/widgets/button.dart
+++ lib/core/widgets/button.dart
@@ -1,53 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_utils/core/sources/colors.dart';
+import 'package:flutter_utils/core/utils/extensions/string_helper.dart';
 
-class Button extends StatelessWidget {
-  final String text;
-  final Color? textColor;
-  final double? textSize;
-  final FontWeight? textStyle;
-  final double? width, height;
-  final double? borderRadius;
-  final EdgeInsetsGeometry? margin, padding;
-  final MaterialStateProperty<Color?>? background;
-  final Function()? onClick;
-
+class Button extends StatefulWidget {
   const Button({
     Key? key,
+    this.width,
+    this.height,
+    this.margin,
+    this.padding,
+    this.background,
+    this.borderRadius,
+    this.enabled = true,
     required this.text,
+    this.textAlign,
+    this.textAllCaps = false,
     this.textColor,
     this.textSize,
     this.textStyle,
-    this.width,
-    this.height,
-    this.background,
-    this.borderRadius,
-    this.margin,
-    this.padding,
+    this.textOverflow,
+    this.letterSpacing,
+    this.lineSpacing,
     this.onClick,
   }) : super(key: key);
 
+  final double? width, height;
+  final EdgeInsetsGeometry? margin, padding;
+  final Color? background;
+  final double? borderRadius;
+  final bool enabled;
+  final String text;
+  final TextAlign? textAlign;
+  final bool textAllCaps;
+  final Color? textColor;
+  final double? textSize;
+  final FontWeight? textStyle;
+  final TextOverflow? textOverflow;
+  final double? letterSpacing;
+  final double? lineSpacing;
+  final Function()? onClick;
+
+  @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: margin,
-      width: width ?? double.infinity,
-      height: height ?? 50,
+      margin: widget.margin,
+      width: widget.width,
+      height: widget.height,
       clipBehavior: Clip.antiAlias,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(borderRadius ?? 0),
+        color: background,
+        borderRadius: BorderRadius.circular(widget.borderRadius ?? 8),
       ),
-      child: ElevatedButton(
-        onPressed: onClick,
-        style: ButtonStyle(backgroundColor: background),
-        child: Text(
-          text,
-          style: TextStyle(
-            color: textColor ?? Colors.white,
-            fontWeight: textStyle ?? FontWeight.bold,
-            fontSize: textSize ?? 18,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: enabled ? widget.onClick : null,
+          child: Container(
+            padding: widget.padding ??
+                const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 12,
+                ),
+            child: Text(
+              text,
+              maxLines: 1,
+              overflow: widget.textOverflow ?? TextOverflow.ellipsis,
+              textAlign: widget.textAlign ?? TextAlign.center,
+              style: TextStyle(
+                color: AppColors.white,
+                fontSize: widget.textSize ?? 16,
+                fontWeight: widget.textStyle,
+                letterSpacing: widget.letterSpacing,
+              ),
+            ),
           ),
         ),
       ),
     );
+  }
+
+  String get text => widget.textAllCaps ? widget.text.uppercase : widget.text;
+
+  bool get enabled => widget.enabled && widget.onClick != null;
+
+  Color get color {
+    final enabledColor = widget.textColor ?? Colors.white;
+    final disabledColor = Colors.grey.shade600;
+    return enabled ? enabledColor : disabledColor;
+  }
+
+  Color get background {
+    final enabledColor = widget.background ?? Theme.of(context).primaryColor;
+    final disabledColor = Color(0xFFFAFAFA);
+    return enabled ? enabledColor : disabledColor;
   }
 }
---
+++ lib/presentation/pages/button_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../core/widgets/button.dart';
+
+class ButtonPage extends StatelessWidget {
+  const ButtonPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Button(
+              text: "Submit",
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 12,
+              ),
+              //onClick: () {},
+              textAllCaps: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
